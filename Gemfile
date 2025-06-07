cfffb33cf2be0f0ccc41dc05c33ab8579507d8ac
hyywon <noddy0501@gmail.com>
2025-06-07 22:08:59 +0900

    feat: sitemap 추가

--- Gemfile
+++ Gemfile
@@ -14,3 +14,5 @@ end
 gem "wdm", "~> 0.2.0", :platforms => [:mingw, :x64_mingw, :mswin]
 
 gem "jekyll", "~> 4.3"
+
+gem "jekyll-sitemap"
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749299541',resources: [
+    cacheName: 'chirpy-1749301702',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
