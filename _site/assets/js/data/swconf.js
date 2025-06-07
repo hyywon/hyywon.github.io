1b1cf247c4cff9e891e1bcc5956e73713d588cc4
hyywon <noddy0501@gmail.com>
2025-06-07 23:23:40 +0900

    feat: meta 복구

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1749301702',resources: [
+    cacheName: 'chirpy-1749306187',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
